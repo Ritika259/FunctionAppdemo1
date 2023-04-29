6590cc1d54b15fff742045d4bc7df3ff3f883d06
Ritika259 <69634876+Ritika259@users.noreply.github.com>
2023-04-29 18:09:44 +0530

    Update Function1.cs
    updated http

--- FunctionAppdemo1/Function1.cs
+++ FunctionAppdemo1/Function1.cs
@@ -27,7 +27,7 @@ namespace FunctionAppdemo1
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello hiiiiiii, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello hiiiiiii, {name}. This HTTP triggered function executed successfully. this is updated";
 
             return new OkObjectResult(responseMessage);
         }
